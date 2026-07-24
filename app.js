f80bcb87305a35f143784e50ffec04c6dba0acf9
soham <soham.acharya9341@gmail.com>
2026-07-24 17:23:40 +0530

    commit 7

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+import { msalInstance } from "./auth.js";
+
 const output = document.getElementById("output");
 
 output.textContent = "JavaScript loaded successfully.";
-
-//
--- auth.js
+++ auth.js
@@ -1,3 +1,5 @@
+import { PublicClientApplication } from "@azure/msal-browser";
+
 const msalConfig = {
     auth: {
         clientId: "2b7a3439-67d5-4cb0-b8d1-a244078196e3",
@@ -6,6 +8,6 @@ const msalConfig = {
     }
 };
 
-const msalInstance = new msal.PublicClientApplication(msalConfig);
+export const msalInstance = new PublicClientApplication(msalConfig);
 
 console.log("MSAL initialized.");
