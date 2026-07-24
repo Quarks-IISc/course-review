af28ed8741eee7936262115fc7092f8eff77aa40
soham <soham.acharya9341@gmail.com>
2026-07-24 20:12:04 +0530

    commit 9.2

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "2b7a3439-67d5-4cb0-b8d1-a244078196e3",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: window.location.href
+        redirectUri: "https://sohamthecoder123.github.io/entra-restricted-2/";
     }
 };
 
