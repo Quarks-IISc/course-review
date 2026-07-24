67bb25d2245c1445da182d4432514adc39108474
soham <soham.acharya9341@gmail.com>
2026-07-24 19:14:16 +0530

    commit 7.5

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import { msalInstance } from "/auth.js";
+import { msalInstance } from "./auth.js";
 
 const output = document.getElementById("output");
 
 output.textContent = "JavaScript loaded successfully.";
+
+console.log(msalInstance);
--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from "https://esm.sh/@azure/msal-browser";
 
 const msalConfig = {
     auth: {
         clientId: "2b7a3439-67d5-4cb0-b8d1-a244078196e3",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: window.location.origin
+        redirectUri: window.location.href
     }
 };
 
