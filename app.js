286cf173ebb3a85823859273964c1c5e3da6c1a9
sohamthecoder123 <soham.acharya9341@gmail.com>
2026-08-02 17:24:28 +0530

    commit 15.1

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ async function initialize() {
 
 async function authorizeUser(idToken) {
     const result = await fetch(
-        "https://hojlmrrgkqvchqyggmbb.supabase.co/functions/v1/get-courses",
+        "https://hojlmrrgkqvchqyggmbb.supabase.co/functions/v1/get-professor",
         {
             method: "POST",
             headers: {
