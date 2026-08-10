79dd14d119335523187848d7ba30f9125e5ca473
sohamthecoder123 <69463653+sohamthecoder123@users.noreply.github.com>
2026-08-10 23:14:25 +0530

    Update redirect URI in msalConfig

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "2b7a3439-67d5-4cb0-b8d1-a244078196e3",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: "https://sohamthecoder123.github.io/entra-restricted-2/"
+        redirectUri: "https://mihirkapse.github.io/entra-restricted-2/"
     }
 };
 
