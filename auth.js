8a17366bd50dd25504876612dbdce1ca1ffe025e
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-08-16 23:23:32 +0530

    config fixes

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "2b7a3439-67d5-4cb0-b8d1-a244078196e3",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: "https://mihirkapse.github.io/entra-restricted-2/"
+        redirectUri: "https://quarks.ug.iisc.ac.in/course-review/"
     }
 };
 
